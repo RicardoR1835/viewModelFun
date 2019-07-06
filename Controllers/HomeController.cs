c7183212d6073bb547d7027ef045ffaeb7fb0199
ricardor1835 <rick.rivera1835@gmail.com>
2019-07-05 20:30:12 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viewModelFun.Models;
+
+namespace viewModelFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            string torender = "This is a sample of a string model yo!";
+
+            return View("Index", torender);
+        }
+
+        [HttpGet("numbers")]
+        public IActionResult Numbers()
+        {
+            int[] numbers = new int[]
+            {
+                1,2,3,4,5,6,9,10,20
+            };
+            return View(numbers);
+        }
+
+        [HttpGet("user")]
+        public IActionResult User(){
+            MyModel rick = new MyModel(){
+                FirstName = "Ricky",
+                LastName = "Raccon"
+            };
+            return View(rick);
+        }
+
+        [HttpGet("users")]
+        public IActionResult Users(){
+            MyModel jonte = new MyModel(){
+                FirstName = "jonte",
+                LastName = "john"
+            };
+            MyModel novi = new MyModel(){
+                FirstName = "Novi",
+                LastName = "Nov"
+            };
+            MyModel Dave = new MyModel(){
+                FirstName = "Dave",
+                LastName = "Gee"
+            };
+            MyModel Juma = new MyModel(){
+                FirstName = "Juma",
+                LastName = "Hamden"
+            };
+            List<MyModel> allUsers = new List<MyModel>()
+            {
+                jonte, novi, Dave, Juma
+            };
+            return View(allUsers);
+        }
+
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace viewModelFun.Models
+{
+    public class MyModel
+    {
+       public string FirstName {get;set;}
+       public string LastName {get;set;}
+    }
+}
